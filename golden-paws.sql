c1586942a38018e1665e82bdf0520f6c6472820b
Susan Benavides <81941175+susan0104@users.noreply.github.com>
2025-12-11 17:18:19 -0600

    Limpieza

--- golden-paws.sql
+++ golden-paws.sql
@@ -37,9 +37,11 @@ INSERT INTO MascotaEspecie (Nombre) values ('Perro');
 INSERT INTO MascotaEspecie (Nombre) values ('Gato');
 INSERT INTO MascotaEspecie (Nombre) values ('Ave');
 
-INSERT INTO Servicio (Nombre, Descripcion, Precio, Duracion_estimada) values ('Revisión General', 'Consulta básica de estado',18500,30);
-INSERT INTO Servicio (Nombre, Descripcion, Precio, Duracion_estimada) values ('Vacunacion Anual', 'Incluye multiple y rabia y distemper',34800,20);
-INSERT INTO Servicio (Nombre, Descripcion, Precio, Duracion_estimada) values ('Baño Grooming', 'Servicio de baño estandar',10000,60);
+INSERT INTO Servicio (Nombre, Descripcion, Precio, Duracion_estimada) 
+VALUES 
+('Revisión General', 'Consulta básica de estado', 18500, 30),
+('Vacunación Anual', 'Incluye múltiple, rabia y distemper', 34800, 20),
+('Baño Grooming', 'Servicio de baño estándar', 10000, 60);
 
 /*TABLAS BASE DE LA VETERINARIA*/
 
@@ -54,34 +56,15 @@ CREATE TABLE Usuario (
   CONSTRAINT FK_USUARIOROL FOREIGN KEY (Rol_Id) REFERENCES UsuarioRol (Rol_Id)
 );
 
-/*LAS INSERCIONES DE LAS TABLAS AUXILIARES*/
-INSERT INTO UsuarioRol (Nombre) values ('Secretaria');
-INSERT INTO UsuarioRol (Nombre) values ('Veterinaria');
-INSERT INTO UsuarioRol (Nombre) values ('Asistente');
-
-INSERT INTO ClientePlan (Nombre) values ('Estándar');
-INSERT INTO ClientePlan (Nombre) values ('Avanzado');
-INSERT INTO ClientePlan (Nombre) values ('Premium');
-
-INSERT INTO MascotaEspecie (Nombre) values ('Perro');
-INSERT INTO MascotaEspecie (Nombre) values ('Gato');
-INSERT INTO MascotaEspecie (Nombre) values ('Ave');
-
-INSERT INTO Servicio (Nombre, Descripcion, Precio, Duracion_estimada) values ('Revisión General', 'Consulta básica de estado',18500,30);
-INSERT INTO Servicio (Nombre, Descripcion, Precio, Duracion_estimada) values ('Vacunacion Anual', 'Incluye multiple y rabia y distemper',34800,20);
-INSERT INTO Servicio (Nombre, Descripcion, Precio, Duracion_estimada) values ('Baño Grooming', 'Servicio de baño estandar',10000,60);
-
-/*TABLAS BASE DE LA VETERINARIA*/
-
-CREATE TABLE Usuario (
-  Usuario_Id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Cliente (
+  Cliente_Id INT AUTO_INCREMENT PRIMARY KEY,
   Nombre VARCHAR(150) NOT NULL,
-  Usuario VARCHAR(80) NOT NULL UNIQUE,
-  Contrasena VARCHAR(255) NOT NULL,
+  Identificacion VARCHAR(20) NOT NULL,
+  FechaNacimiento DATE NOT NULL,
   Correo VARCHAR(100) NOT NULL,
-  Rol_Id SMALLINT NOT NULL,
+  Plan_Id SMALLINT NOT NULL,
   Fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT FK_USUARIOROL FOREIGN KEY (Rol_Id) REFERENCES UsuarioRol (Rol_Id)
+  CONSTRAINT FK_CLIENTEPLAN FOREIGN KEY (Plan_Id) REFERENCES ClientePlan (Plan_Id)
 );
 
 CREATE TABLE Mascota (
@@ -93,10 +76,10 @@ CREATE TABLE Mascota (
   Observaciones varchar(200) NOT NULL,
   Fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP,
   Cliente_Id INT NOT NULL,
+  Foto LONGTEXT NOT NULL,
   CONSTRAINT FK_MASCOTAESPECIE FOREIGN KEY (Especie_Id) REFERENCES MascotaEspecie (Especie_Id),
   CONSTRAINT FK_MASCOTACLIENTE FOREIGN KEY (Cliente_Id) REFERENCES Cliente (Cliente_Id)
 );
-
 
 CREATE TABLE MascotaExpediente (
   Expediente_Id INT AUTO_INCREMENT PRIMARY KEY,
@@ -110,11 +93,12 @@ CREATE TABLE MascotaExpediente (
   CONSTRAINT fk_expediente_mascota FOREIGN KEY (Mascota_Id) REFERENCES Mascota(Mascota_Id)
 );
 
+
 CREATE TABLE Citas (
   Cita_Id INT AUTO_INCREMENT PRIMARY KEY,
   Mascota_Id INT NOT NULL,
   Servicio_Id INT NOT NULL,
-  Usuario_Id INT NOT NULL,
+  Usuario_Id INT,
   Fecha DATETIME NOT NULL,
   Estado VARCHAR(50) DEFAULT 'Programada',
   Precio DECIMAL(10,2),
@@ -126,4 +110,3 @@ CREATE TABLE Citas (
 
   CONSTRAINT fk_cita_usuario FOREIGN KEY (Usuario_Id) REFERENCES Usuario (Usuario_Id)
 );
-
